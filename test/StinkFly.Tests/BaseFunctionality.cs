7856389d75ce92351779cacab63135c35cf07289
Chris Ortman <chrisortman@martin-group.com>
2009-01-15 18:23:10 -0600

    Url with a variable

--- test/StinkFly.Tests/BaseFunctionality.cs
+++ test/StinkFly.Tests/BaseFunctionality.cs
@@ -2,35 +2,47 @@ namespace StinkFly.Tests
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Linq;
 
 	using Xunit;
 
 
 
-	public class StinkFlyProcessor
+	public class StinkFlyProcessor : IBuilder
 	{
-		private Dictionary<string, Func<string>> _lookup;
+		private Dictionary<string, Func<CallContext,string>> _lookup;
 		public StinkFlyProcessor()
 		{
-			_lookup = new Dictionary<string, Func<string>>();
+			_lookup = new Dictionary<string, Func<CallContext,string>>();
 		}
 
-		public void Add(string url, Func<string> action)
+		public void Add(string url, Func<CallContext,string> action)
 		{
 			_lookup.Add(url, action);
 		}
 
-		public Func<URL> Builder(string url, Func<string> action)
+		
+
+		public Func<URL> Build(string url, Func<CallContext, string> action)
 		{
-			Add(url, action);
+			_lookup.Add(url, action);
 			Func<URL> returnValue = () => new URL();
+			return returnValue;
+		}
+
+		public Func<PARAM,URL> Build<PARAM>(string url, Func<CallContext, string> action)
+		{
+			_lookup.Add(url, action);
+			Func<PARAM,URL> returnValue = x => new URL();
 			return returnValue;
 		}
 
 		public string Process(string url)
 		{
-			var func = _lookup[url];
-			return func();
+
+			var func = _lookup.ContainsKey(url) ? _lookup[url] : _lookup.Values.First();
+			var context = new CallContext();
+			return func(context);
 		}
 	}
 
@@ -38,23 +50,52 @@ namespace StinkFly.Tests
 	{
 		
 	}
+
+	public interface IBuilder
+	{
+		Func<URL> Build(string url, Func<CallContext, string> action);
+		Func<PARAM, URL> Build<PARAM>(string url, Func<CallContext, string> action);
+	}
+
+
+	public class CallContext
+	{
+		public IDictionary<string,object> Params { get; set;}
+		public CallContext()
+		{
+			Params = new Dictionary<string, object>();
+			Params["name"] = "chris";
+		}
+	}
 	public class StinkFlyApplication
 	{
-		public static Func<string, Func<string>, Func<URL>> Builder { get; set;}
+		public static IBuilder Builder { get; set;}
 		
 		protected static Func<URL> get(string url, Func<string> action)
 		{
-			return Builder(url, action);
+			Func<CallContext, string> executor = ctx => action();
+			return Builder.Build(url, executor);
 			
+		}
+
+		protected static Func<PARAM,URL> get<PARAM>(string url, Func<PARAM,string> action)
+		{
+			Func<CallContext, string> executor = ctx =>
+			                                     {
+			                                     	var paramValue = (PARAM) ctx.Params["name"];
+			                                     	return action(paramValue);
+			                                     };
+			
+			return Builder.Build<PARAM>(url, executor);
 		}
 	}
 
 
-	public class BaseFunctionality
+	public class Executing_urls_with_anonymous_methods
 	{
 		
 		
-		public class Can_take_a_url_and_return_a_string : Spec
+		public class a_static_url : Spec
 		{
 			
 			class MyApp : StinkFlyApplication
@@ -66,10 +107,8 @@ namespace StinkFly.Tests
 			private string _response;
 			public override void EstablishContext() 
 			{
-
-				
 				StinkFlyProcessor processor = new StinkFlyProcessor();
-				StinkFlyApplication.Builder = processor.Builder;
+				StinkFlyApplication.Builder = processor;
 
 				var app = new MyApp();
 
@@ -77,9 +116,34 @@ namespace StinkFly.Tests
 			}
 
 			[Observation]
-			public void Response_should_be_hello_world()
+			public void Response_should_be_able_to_respond()
 			{
 				Assert.Equal("Hello World",_response);
+			}
+		}
+
+		public class a_url_with_a_a_variable : Spec
+		{
+			class MyApp : StinkFlyApplication
+			{
+				private Func<string,URL> hello_url = get<string>("hello/{name}", name => "Hello " + name);
+			}
+
+			private string _response;
+
+			public override void EstablishContext() {
+				StinkFlyProcessor processor = new StinkFlyProcessor();
+				StinkFlyApplication.Builder = processor;
+
+				var app = new MyApp();
+				_response = processor.Process("hello/chris");
+
+			}
+
+			[Observation]
+			public void be_able_to_pass_the_variables_value_to_the_method()
+			{
+				Assert.Equal("Hello chris",_response);
 			}
 		}
 	}
